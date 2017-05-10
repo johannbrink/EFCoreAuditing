ebbaa92fd2f579f84577083d1be297f97cafe048
Johann Brink <johann.brink@outlook.com>
2017-05-10 21:07:03 +0200

    Added base code

---
+++ src/EFCoreAuditing.Test/Customer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace EFCoreAuditing.Test
+{
+    public class Customer
+    {
+        [Key]
+        public int CustomerId { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        [Timestamp]
+        [DoNotAudit]
+        public byte[] ConcurrencyStamp { get; set; }
+    }
+}
---
+++ src/EFCoreAuditing.Test/MyDBContext.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EFCoreAuditing.Test
+{
+    public class MyDBContext : AuditingDbContext
+    {
+        public MyDBContext()
+        {
+
+        }
+
+        public MyDBContext(IExternalAuditStoreProvider externalAuditStoreProvider) : base(externalAuditStoreProvider)
+        {
+
+        }
+
+        public DbSet<Customer> Customers { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseInMemoryDatabase();//.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=EFAuditingTestHarness;Trusted_Connection=True;MultipleActiveResultSets=true");
+            base.OnConfiguring(optionsBuilder);
+        }
+        //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        //{
+        //    optionsBuilder.UseSqlServer(
+        //        "Server=(localdb)\\mssqllocaldb;Database=EFAuditingTestHarness;Trusted_Connection=True;MultipleActiveResultSets=true");
+        //}
+    }
+}
